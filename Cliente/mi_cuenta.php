91796ad85bbe2bda5b4311cde164736fd5c46673
DulceGA01 <dulcega1201@gmail.com>
2023-03-06 18:03:37 -0600

    modi060323

--- Cliente/mi_cuenta.php
+++ Cliente/mi_cuenta.php
@@ -13,118 +13,120 @@
   </head>
   <body>
     <div class="registropago">
-      <div class="row">
-        <div class="col-12 col-md-3">
-          <div class="logo">
-            <img
-              src="../img/selfish-logo-6A0845C041-seeklogo.com.png"
-              alt="Logo"
-              width="90px"
-            />
-          </div>
-          <div class="perfil">
-            <p>Dulce Garcia</p>
-          </div>
-          <div id="wrapperMenu">
-            <ul id="menu-dashboard" class="nav">
-              <li class="dropdown">
-                <a
-                  href="#"
-                  class="dropdown-toggle"
-                  data-toggle="collapse"
-                  data-target="#submenu"
-                  aria-expanded="false"
-                >
-                  <span class="caret"
-                    ><i class="fa fa-th-list" aria-hidden="true"></i> Mis
-                    Proyectos</span
-                  >
-                </a>
-                <ul
-                  class="nav collapse"
-                  id="submenu"
-                  role="menu"
-                  aria-labelledby="btn-1"
-                >
-                  <li><a href="#">Proyecto1</a></li>
-                  <li><a href="#">Proyecto2</a></li>
-                  <li><a href="#">Proyecto3</a></li>
-                  <li><a href="#">Proyecto4</a></li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">
-                  <span class="hidden-xs"
-                    ><i class="fa fa-user" aria-hidden="true"></i> Mi
-                    cuenta</span
-                  >
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span class="hidden-xs"
-                    ><i class="fa fa-bell" aria-hidden="true"></i>
-                    Notificaciones</span
-                  >
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span class="hidden-xs"
-                    ><i class="fa fa-sign-out" aria-hidden="true"></i> Cerrar
-                    Sesion</span
-                  >
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-12 col-md-7">
-          <div class="cabecera">
-            <div class="misdatos">
-              <h1>MI CUENTA</h1>
-              <p>"Bienvenid@"</p>
+      <div class="micuenta">
+        <div class="row">
+          <div class="col-12 col-md-3">
+            <div class="logo">
+              <img
+                src="../img/selfish-logo-6A0845C041-seeklogo.com.png"
+                alt="Logo"
+                width="90px"
+              />
             </div>
-            <div class="fechahoy">
-              <p>03/03/2023</p>
+            <div class="perfil">
+              <p>Dulce Garcia</p>
+            </div>
+            <div id="wrapperMenu">
+              <ul id="menu-dashboard" class="nav">
+                <li class="dropdown">
+                  <a
+                    href="#"
+                    class="dropdown-toggle"
+                    data-toggle="collapse"
+                    data-target="#submenu"
+                    aria-expanded="false"
+                  >
+                    <span class="caret"
+                      ><i class="fa fa-th-list" aria-hidden="true"></i> Mis
+                      Proyectos</span
+                    >
+                  </a>
+                  <ul
+                    class="nav collapse"
+                    id="submenu"
+                    role="menu"
+                    aria-labelledby="btn-1"
+                  >
+                    <li><a href="#">Proyecto1</a></li>
+                    <li><a href="#">Proyecto2</a></li>
+                    <li><a href="#">Proyecto3</a></li>
+                    <li><a href="#">Proyecto4</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="hidden-xs"
+                      ><i class="fa fa-user" aria-hidden="true"></i> Mi
+                      cuenta</span
+                    >
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="hidden-xs"
+                      ><i class="fa fa-bell" aria-hidden="true"></i>
+                      Notificaciones</span
+                    >
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="hidden-xs"
+                      ><i class="fa fa-sign-out" aria-hidden="true"></i> Cerrar
+                      Sesion</span
+                    >
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
-          <div class="con">
-            <div class="cuenta">
-              <div class="avatar">
-                <img class="imagen" src="../img/images.jpg" alt="Logo" />
+          <div class="col-12 col-md-7">
+            <div class="cabecera">
+              <div class="misdatos">
+                <h1>MI CUENTA</h1>
+                <p>"Bienvenid@"</p>
               </div>
-              <div class="datospersonales">
-                <h4>Nombre Apellido</h4>
-                <h4>7721547717</h4>
-                <h4>dulcega1201@gmail.com</h4>
-                <h4>24/12/2001</h4>
+              <div class="fechahoy">
+                <p>03/03/2023</p>
               </div>
             </div>
-            <div class="accesos">
-              <div class="fatn">
-                <button class="btnn">
-                  <a href="#"
-                    ><i class="fa fa-indent" aria-hidden="true"></i> Agregar
-                    Dirección de Facuración</a
-                  >
-                </button>
+            <div class="con">
+              <div class="cuenta">
+                <div class="avatar">
+                  <img class="imagen" src="../img/images.jpg" alt="Logo" />
+                </div>
+                <div class="datospersonales">
+                  <h4>Nombre Apellido</h4>
+                  <h4>7721547717</h4>
+                  <h4>dulcega1201@gmail.com</h4>
+                  <h4>24/12/2001</h4>
+                </div>
               </div>
-              <div class="btns">
-                <div class="cabtn">
-                  <button class="btn">
+              <div class="accesos">
+                <div class="fatn">
+                  <button class="btnn">
                     <a href="#"
-                      ><i class="fa fa-money" aria-hidden="true"></i> Cambiar
-                      Contraseña</a
+                      ><i class="fa fa-indent" aria-hidden="true"></i> Agregar
+                      Dirección de Facuración</a
                     >
                   </button>
                 </div>
-                <div class="pabtn">
-                  <button class="btn">
-                    <a href="#"
-                      ><i class="fa fa-money" aria-hidden="true"></i> Pago</a
-                    >
-                  </button>
+                <div class="btns">
+                  <div class="cabtn">
+                    <button class="btn">
+                      <a href="#"
+                        ><i class="fa fa-money" aria-hidden="true"></i> Cambiar
+                        Contraseña</a
+                      >
+                    </button>
+                  </div>
+                  <div class="pabtn">
+                    <button class="btn">
+                      <a href="#"
+                        ><i class="fa fa-money" aria-hidden="true"></i> Pago</a
+                      >
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
